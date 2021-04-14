79e72177a4e1cac87aa69fb0dea5869542daf4a6
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-14 14:37:02 +0100

    Initial Database setup
    Created Database Context and added an initial Employee Table

---
+++ Data/CinemaPortalCoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CinemaPortalCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaPortalCore.Data
+{
+    public class CinemaPortalCoreDbContext : DbContext
+    {
+        public CinemaPortalCoreDbContext(DbContextOptions<CinemaPortalCoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
---
+++ Migrations/20210414133620_addEmployeesToDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CinemaPortalCore.Migrations
+{
+    public partial class addEmployeesToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Employee_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Employee_ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
