1df089848487e0433de9c49f265b0e4b1cba757d
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-15 13:04:01 +0100

    Tidy up form display
    Just added some display names to properties in movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -13,18 +13,29 @@ namespace CinemaPortalCore.Models
         public int Movie_ID { get; set; }
 
         public string Title { get; set; }
+
         public string Video { get; set; }
+
         public string Image { get; set; }
+
         public string Description { get; set; }
 
         [DisplayName("Release date")]
         public string ReleaseDate { get; set; }
 
+        [DisplayName("Age rating")]
         public string AgeRating { get; set; }
+
+        [DisplayName("Run time")]
         public string RunTime { get; set; }
+
         public string Genre { get; set; }
+
         public string Director { get; set; }
+
         public string Actors { get; set; }
+
+        [DisplayName("Star rating")]
         public string StarRating { get; set; }
     }
 }
