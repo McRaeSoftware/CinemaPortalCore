5702e8f32a525d71ae2c02c4e390e4cdf359d9e1
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-17 18:29:58 +0100

    Update Employees
    added update employee functionality and promote, demote employee button functionality

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -36,10 +36,40 @@ namespace CinemaPortalCore.Controllers
             return View();
         }
 
+        // Get employee details for update
+        public IActionResult UpdateEmployee(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var employee = _database.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // Update employee details after post from updateEmployee
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _database.Employees.Update(employee);
+                _database.SaveChanges();
+            }
+            return View();
+        }
+
         // Get employee details to delete
         public IActionResult DeleteEmployee(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
@@ -59,13 +89,52 @@ namespace CinemaPortalCore.Controllers
         public IActionResult DeleteEmployeePOST(int Employee_ID)
         {
             var employee = _database.Employees.Find(Employee_ID);
-
-            if(employee == null)
+            if (employee == null)
             {
                 return NotFound();
             }
 
             _database.Employees.Remove(employee);
+            _database.SaveChanges();
+
+            return RedirectToAction("ManageEmployee");
+        }
+
+        public IActionResult PromoteEmployee(int id)
+        {
+            string newJobrole;
+            var employee = _database.Employees.Find(id);
+
+            if (employee.Jobrole == "employee")
+            {
+                newJobrole = "supervisor";
+            }
+            else
+            {
+                newJobrole = "manager";
+            }
+
+            employee.Jobrole = newJobrole;
+            _database.SaveChanges();
+
+            return RedirectToAction("ManageEmployee");
+        }
+
+        public IActionResult DemoteEmployee(int id)
+        {
+            string newJobrole;
+            var employee = _database.Employees.Find(id);
+
+            if (employee.Jobrole == "manager")
+            {
+                newJobrole = "supervisor";
+            }
+            else
+            {
+                newJobrole = "employee";
+            }
+
+            employee.Jobrole = newJobrole;
             _database.SaveChanges();
 
             return RedirectToAction("ManageEmployee");
