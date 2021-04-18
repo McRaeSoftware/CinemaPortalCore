a24139bfd65f640a9173534ab3e1cbe73775bc35
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-18 23:02:12 +0100

    added Manage Customer screen
    Can now view customers on the manage customer View
    Also added navigation button to the views on the shared layout page

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using CinemaPortalCore.Data;
+using CinemaPortalCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaPortalCore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CinemaPortalCoreDbContext _database;
+
+        public CustomerController(CinemaPortalCoreDbContext database)
+        {
+            _database = database;
+        }
+
+        public IActionResult ManageCustomer()
+        {
+            IEnumerable<Customer> customerList = _database.Customers;
+            return View(customerList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _database.Customers.Add(customer);
+                _database.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
