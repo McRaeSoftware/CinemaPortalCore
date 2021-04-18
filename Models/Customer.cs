f9970c97f64b8bf357cacf5f8ffc849646150ee1
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-18 20:48:16 +0100

    Created Customer class
    Customer class has been added along with some validation

---
+++ Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaPortalCore.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Customer_ID { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name can only contain letters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Surname can only contain letters.")]
+        public string Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain letters and numbers.")]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,30}$", ErrorMessage = "Password must be between 8 and 30 characters and contain atleast 1 lowercase, uppercase a number and symbol")]
+        public string Password { get; set; }
+    }
+}
