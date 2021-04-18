9277b9bab9d56a854cc2fb354438fb4ea586fa14
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-18 12:48:31 +0100

    Movie property validation
    added some validation to the Movie table

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,29 +12,41 @@ namespace CinemaPortalCore.Models
         [Key]
         public int Movie_ID { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Video { get; set; }
 
+        [Required]
         public string Image { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         [DisplayName("Release date")]
         public string ReleaseDate { get; set; }
 
+        [Required]
         [DisplayName("Age rating")]
         public string AgeRating { get; set; }
 
+        [Required]
         [DisplayName("Run time")]
         public string RunTime { get; set; }
 
+        [Required]
         public string Genre { get; set; }
 
+        [Required]
         public string Director { get; set; }
 
+        [Required]
         public string Actors { get; set; }
 
+        [Required]
+        [Range(0, 5)]
         [DisplayName("Star rating")]
         public string StarRating { get; set; }
     }
