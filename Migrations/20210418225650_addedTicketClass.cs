9a0d83cfb15a33a0b38cc06864982ec1199a0166
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2021-04-18 23:57:47 +0100

    Ticket Migration
    Added ticket table to database

---
+++ Migrations/20210418225650_addedTicketClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CinemaPortalCore.Migrations
+{
+    public partial class addedTicketClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
